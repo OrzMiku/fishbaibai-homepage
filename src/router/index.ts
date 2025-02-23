dbb0ad28649add86ee2e2705c542335033236d10
OrzMiku <miku@ecy.pink>
2025-02-23 10:53:30 +0800

    Add title to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/', component: () => import('@/views/Home.vue')},
-        {path: '/about', component: () => import('@/views/About.vue')}
+        {path: '/', component: () => import('@/views/Home.vue'), meta: {title: '首页'}},
+        {path: '/about', component: () => import('@/views/About.vue'), meta: {title: '关于'}},
     ]
+});
+
+router.beforeEach((to, _, next) => {
+    window.document.title = `FishBai - ${to.meta.title as string || '鱼摆摆祈祷中...'}`;
+    next();
 });
 
 export default router;
